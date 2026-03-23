e51a752d7cbf0682a5a27e74853bf55d11f7ac64
mohit-verma2000 <mv046070@gmail.com>
2026-03-23 19:59:05 +0000

    Practice Session

---
+++ basics/test.js
@@ -0,0 +1,10 @@
+const accountId=11111
+let accountEmail = "mohit@gmail.com"
+var accountPassword = "12345"
+accountCity="Gwalior"
+
+accountEmail="mohit@google.com"
+accountPassword="112211"
+accountCity="Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
